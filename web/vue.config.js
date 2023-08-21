15489ee23bec6484cc0520c21c2af623fa11eebc
quanminzhe <2602671329@qq.com>
2023-08-21 12:49:36 +0800

    项目启动端口修改为9001

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    port: 9001
+  },
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: './'
